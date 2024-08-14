446e32e65363f940e4b3411c318dc4a117da37dc
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-08-14 20:45:25 -0300

    feat(Initialize main screen): :sparkles:

---
+++ src/screens/Home/home.screen.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+export const Home = () => {
+    return <Text style={{fontSize: 50}}>Hello, home!</Text>
+};
+
+export default Home;
