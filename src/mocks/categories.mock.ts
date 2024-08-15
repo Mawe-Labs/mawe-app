a6e12d597f6bf38212edf7b0d046647bb98bc1b6
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-08-15 19:50:26 -0300

    refactor(Created ctaegories mock): :recycle:

---
+++ src/mocks/categories.mock.ts
@@ -0,0 +1,46 @@
+export const categories = [
+  {
+    id: 1,
+    name: 'Todos'
+  },
+  {
+    id: 2,
+    name: 'Bazar e Limpeza'
+  },
+  {
+    id: 3,
+    name: 'Bebidas'
+  },
+  {
+    id: 4,
+    name: 'Refeições prontas'
+  },
+  {
+    id: 5,
+    name: 'Geladeira'
+  },
+  {
+    id: 6,
+    name: 'Frutas e Vegetais'
+  },
+  {
+    id: 7,
+    name: 'Higiene pessoal'
+  },
+  {
+    id: 8,
+    name: 'Importados'
+  },
+  {
+    id: 9,
+    name: 'Mercearia'
+  },
+  {
+    id: 10,
+    name: 'Padaria'
+  },
+  {
+    id: 11,
+    name: 'Saúde e Beleza'
+  },
+];
