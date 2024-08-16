804850e48a732cdde81459c6d954addcf3f6c80f
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-08-15 21:02:01 -0300

    refactor(Created product mocks & product image): :recycle:

--- src/mocks/categories.mock.ts
+++ src/mocks/categories.mock.ts
@@ -1,46 +1,181 @@
+import banana from '../assets/products/banana.png';
+
 export const categories = [
   {
     id: 1,
-    name: 'Todos'
+    name: 'Todos',
+    products: [
+      {
+        id: 1,
+        image: banana,
+        name: 'Banana',
+        value: 20,
+      },
+      {
+        id: 2,
+        image: '',
+        name: 'Limão',
+        value: 20,
+      },
+      {
+        id: 3,
+        image: '',
+        name: 'Milho',
+        value: 20,
+      },
+      {
+        id: 4,
+        image: '',
+        name: 'Carne',
+        value: 20,
+      },
+      {
+        id: 5,
+        image: '',
+        name: 'Salsicha',
+        value: 20,
+      },
+      {
+        id: 6,
+        image: '',
+        name: 'Cenoura',
+        value: 20,
+      },
+      {
+        id: 7,
+        image: '',
+        name: 'Detergente',
+        value: 20,
+      },
+      {
+        id: 8,
+        image: '',
+        name: 'Coca',
+        value: 20,
+      },
+      {
+        id: 9,
+        image: '',
+        name: 'Pipoca',
+        value: 20,
+      },
+      {
+        id: 10,
+        image: '',
+        name: 'Queijo ralado',
+        value: 20,
+      },
+    ],
   },
   {
     id: 2,
-    name: 'Bazar e Limpeza'
+    name: 'Bazar e Limpeza',
+    products: [
+      {
+        id: 1,
+        image: '',
+        name: 'Detergente',
+        value: 20,
+      },
+    ],
   },
   {
     id: 3,
-    name: 'Bebidas'
+    name: 'Bebidas',
+    products: [
+      {
+        id: 1,
+        image: '',
+        name: 'Coca',
+        value: 20,
+      },
+    ],
   },
   {
     id: 4,
-    name: 'Refeições prontas'
+    name: 'Geladeira',
+    products: [
+      {
+        id: 1,
+        image: '',
+        name: 'Carne',
+        value: 20,
+      },
+      {
+        id: 2,
+        image: '',
+        name: 'Salsicha',
+        value: 20,
+      },
+    ],
   },
   {
     id: 5,
-    name: 'Geladeira'
+    name: 'Frutas e Vegetais',
+    products: [
+      {
+        id: 1,
+        image: banana,
+        name: 'Banana',
+        value: 20,
+      },
+      {
+        id: 2,
+        image: '',
+        name: 'Limão',
+        value: 20,
+      },
+      {
+        id: 3,
+        image: '',
+        name: 'Cenoura',
+        value: 20,
+      },
+    ],
   },
   {
     id: 6,
-    name: 'Frutas e Vegetais'
+    name: 'Higiene pessoal',
+    products: [],
   },
   {
     id: 7,
-    name: 'Higiene pessoal'
+    name: 'Importados',
+    products: [],
   },
   {
     id: 8,
-    name: 'Importados'
+    name: 'Mercearia',
+    products: [
+      {
+        id: 1,
+        image: '',
+        name: 'Pipoca',
+        value: 20,
+      },
+      {
+        id: 2,
+        image: '',
+        name: 'Queijo ralado',
+        value: 20,
+      },
+    ],
   },
   {
     id: 9,
-    name: 'Mercearia'
+    name: 'Padaria',
+    products: [
+      {
+        id: 1,
+        image: '',
+        name: 'Pão',
+        value: 20,
+      },
+    ],
   },
   {
     id: 10,
-    name: 'Padaria'
-  },
-  {
-    id: 11,
-    name: 'Saúde e Beleza'
+    name: 'Saúde e Beleza',
+    products: [],
   },
 ];
