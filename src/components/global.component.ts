1254c86625cff546919e8373d46737deb2dfcb5c
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-08-30 19:50:37 -0300

    style(Changed Circle to global styles): :lipstick:

--- src/components/global.component.ts
+++ src/components/global.component.ts
@@ -1,8 +1,15 @@
 import styled from 'styled-components/native';
+import {themes} from '../styles/themes.styles';
 
 type SpacerProps = {
   size: number;
 };
+
+type CircleProps = {
+  bottom: number;
+};
+
+const {colors} = themes;
 
 export const Spacer = styled.View<SpacerProps>`
   height: ${(props) => `${props.size}px`};
@@ -11,4 +18,14 @@ export const Spacer = styled.View<SpacerProps>`
 export const Flex = styled.View`
   flex-direction: row;
   align-items: center;
+`;
+
+export const CircleAddedItem = styled.TouchableOpacity<CircleProps>`
+  position: absolute;
+  bottom: ${props => `${props.bottom}%`};
+  right: 0;
+  margin-right: 10px;
+  padding: 25px;
+  background-color: ${colors.info};
+  border-radius: 50px;
 `;
