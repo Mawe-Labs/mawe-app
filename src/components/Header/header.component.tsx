d655502c71fd3392da0607b3c4f8f2577a9e7de0
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-08-30 19:12:14 -0300

    feat(Changed to open drawer): :sparkles:

--- src/components/Header/header.component.tsx
+++ src/components/Header/header.component.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import {HeaderContainer, HeaderTitle} from './header.styles';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {faBars} from '@fortawesome/free-solid-svg-icons/faBars';
+import {Pressable} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {DrawerActions} from "@react-navigation/native"
 
 interface HeaderProps {
   title: string;
@@ -9,10 +12,19 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({title, isMyList}) => {
+  const navigation = useNavigation();
+
   return (
     <HeaderContainer>
-      {isMyList && <FontAwesomeIcon icon={faBars} size={30} color="white" />}
-      <HeaderTitle>{title}</HeaderTitle>
+      {isMyList && (
+        <>
+          <Pressable
+            onPress={() => navigation.dispatch(DrawerActions.openDrawer())}>
+            <FontAwesomeIcon icon={faBars} size={30} color="white" />
+          </Pressable>
+          <HeaderTitle>{title}</HeaderTitle>
+        </>
+      )}
     </HeaderContainer>
   );
 };
