0b9a3d680256b68cc59a2cc72fed78dca978637a
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-08-30 19:52:16 -0300

    feat(Created lists screen): :sparkles:

---
+++ src/screens/Lists/list.styles.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components/native';
+
+export const AddedContainer = styled.View`
+  display: flex;
+  height: 100%;
+`;
+
+export const List = styled.View`
+  padding: 20px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ListText = styled.Text`
+  font-size: 20px;
+`;
+
+export const ListCount = styled.Text`
+  font-size: 15px;
+`;
+
+export const LeftList = styled.View`
+  flex-direction: row;
+  align-items: center;
+  gap: 30px;
+`;
