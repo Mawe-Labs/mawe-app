94aa440ab6f2967758bf7dd0a58007bd2ef715ba
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-08-30 19:11:56 -0300

    feat(Added drawer navigation): :sparkles:

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
 import Home from './screens/Home/home.screen';
+import NewItemScreen from './screens/NewItem/new-item.screen';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faHome} from '@fortawesome/free-solid-svg-icons/faHome';
+
+const Drawer = createDrawerNavigator();
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView>
-      <Home />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Drawer.Navigator
+        initialRouteName="Home"
+        screenOptions={{headerShown: false}}>
+        <Drawer.Screen
+          name="Home"
+          component={Home}
+          options={{
+            drawerIcon: () => (
+              <FontAwesomeIcon icon={faHome} size={20} color="gray" />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="NewItem"
+          component={NewItemScreen}
+          options={{
+            drawerLabel: 'Novo item',
+          }}
+        />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
